a2097f0f9dac385bd0303241a37125221aec97c0
Davi César <56692847+Redohairi@users.noreply.github.com>
2024-12-07 14:50:26 -0300

    Update nome.hpp

--- dominios/nome.hpp
+++ dominios/nome.hpp
@@ -6,19 +6,40 @@
 
 using namespace std;
 
+///
+/// Padrao para a representacao do nome
+///
 class Nome {
     private:
-            /// Valida e armazena um valor para nome
-            string valor; //Atributo para armazenar valor.
-            void validar(string); //M�todo para validar valor.
+            ///
+            /// Armazena o valor do nome.
+            ///
+            string valor;
+
+            ///
+            /// Valida o valor do nome conforme as regras definidas.
+            ///
+            /// @param valor Valor do nome a ser validado.
+            /// @throws invalid_argument Caso o valor não obedeça as regras de formatação.
+            ///
+            void validar(string);
     public:
-            /// Atribui um valor para nome
-            void setValor(string); //M�todo para atribuir valor.
-            /// Recupera valor atribuido
-            string getValor() const; // M�todo para recuperar valor.
+            ///
+            /// Atribui um valor ao nome, caso seja válido.
+            ///
+            /// @param valor Valor do nome.
+            ///
+            void setValor(string);
+
+            ///
+            /// Recupera o valor atribuído ao nome.
+            ///
+            /// @return Valor do nome.
+            ///
+            string getValor() const;
 };
 
-inline string Nome::getValor() const { //Implementa��o de m�todo
+inline string Nome::getValor() const {
     return valor;
 }
 
