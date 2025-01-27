1db831f9a32e23771f53d54ab12c7f60fd2dfc59
Luiz Enéas <luiz.felipe.eneas@gmail.com>
2025-01-27 09:53:35 -0300

    alteração nomes pasta e arquivos

--- stubs/sisautenticacao.hpp
+++ stubs/sisautenticacao.hpp
@@ -5,7 +5,7 @@
 #include "../dominios/codigo.hpp"
 #include "../dominios/senha.hpp"
 #include "../interfaces/isautenticacao.hpp"
-#include "../conteiners/conteiner.hpp"
+#include "../containers/csconta.hpp"
 
 using namespace std;
 
@@ -18,53 +18,53 @@ using namespace std;
  * conforme o código e a senha informados.
  */
 class StubISAutenticacao: public ISAutenticacao {
-private:
-    ContainerConta container; ///< Contêiner interno de contas
+    private:
+        ContainerConta container; ///< Contêiner interno de contas
 
-public:
+    public:
 
-    /**
-     * @brief Construtor da classe StubISAutenticacao.
-     *
-     * Popula o contêiner com algumas contas de teste. Caso alguma dessas contas
-     * seja inválida (por exemplo, senha em sequência, código fora do padrão),
-     * é lançada uma exceção @c invalid_argument, que é capturada e tratada
-     * internamente, exibindo uma mensagem de erro.
-     */
-    StubISAutenticacao();
+        /**
+         * @brief Construtor da classe StubISAutenticacao.
+         *
+         * Popula o contêiner com algumas contas de teste. Caso alguma dessas contas
+         * seja inválida (por exemplo, senha em sequência, código fora do padrão),
+         * é lançada uma exceção @c invalid_argument, que é capturada e tratada
+         * internamente, exibindo uma mensagem de erro.
+         */
+        StubISAutenticacao();
 
-    /**
-     * @brief Gatilho de falha para simular erro de autenticação.
-     *
-     * Se o código for igual a @c "CODFLH", o método de autenticação retornará false.
-     */
-    const string TRIGGER_FALHA = "CODFLH";
+        /**
+         * @brief Gatilho de falha para simular erro de autenticação.
+         *
+         * Se o código for igual a @c "CODFLH", o método de autenticação retornará false.
+         */
+        const string TRIGGER_FALHA = "CODFLH";
 
-    /**
-     * @brief Gatilho de erro de sistema.
-     *
-     * Se o código for igual a @c "CODERR", o método de autenticação lançará
-     * um @c runtime_error, simulando um erro interno no sistema.
-     */
-    const string TRIGGER_ERRO_SISTEMA = "CODERR";
+        /**
+         * @brief Gatilho de erro de sistema.
+         *
+         * Se o código for igual a @c "CODERR", o método de autenticação lançará
+         * um @c runtime_error, simulando um erro interno no sistema.
+         */
+        const string TRIGGER_ERRO_SISTEMA = "CODERR";
 
-    /**
-     * @brief Método que autentica o usuário, de acordo com a interface ISAutenticacao.
-     *
-     * - Verifica se @p codigo é igual a @ref TRIGGER_FALHA; se for, retorna false.
-     * - Verifica se @p codigo é igual a @ref TRIGGER_ERRO_SISTEMA; se for, lança
-     *   exceção @c runtime_error.
-     * - Caso contrário, pesquisa no contêiner uma conta com esse código. Se achar,
-     *   compara a senha. Se a senha bater, retorna true; caso contrário, false.
-     *
-     * @param codigo Objeto do domínio Codigo, representando o código de usuário.
-     * @param senha Objeto do domínio Senha, representando a senha do usuário.
-     * @return true se a autenticação for bem-sucedida,
-     * @return false se falhar (não encontrou conta ou a senha não confere).
-     *
-     * @throw runtime_error caso o código seja @ref TRIGGER_ERRO_SISTEMA.
-     */
-    bool autenticar(const Codigo &codigo, const Senha &senha) override;
+        /**
+         * @brief Método que autentica o usuário, de acordo com a interface ISAutenticacao.
+         *
+         * - Verifica se @p codigo é igual a @ref TRIGGER_FALHA; se for, retorna false.
+         * - Verifica se @p codigo é igual a @ref TRIGGER_ERRO_SISTEMA; se for, lança
+         *   exceção @c runtime_error.
+         * - Caso contrário, pesquisa no contêiner uma conta com esse código. Se achar,
+         *   compara a senha. Se a senha bater, retorna true; caso contrário, false.
+         *
+         * @param codigo Objeto do domínio Codigo, representando o código de usuário.
+         * @param senha Objeto do domínio Senha, representando a senha do usuário.
+         * @return true se a autenticação for bem-sucedida,
+         * @return false se falhar (não encontrou conta ou a senha não confere).
+         *
+         * @throw runtime_error caso o código seja @ref TRIGGER_ERRO_SISTEMA.
+         */
+        bool autenticar(const Codigo &codigo, const Senha &senha) override;
 };
 
 #endif // SISAUTENTICACAO_HPP_INCLUDED
---
+++ stubs/sisautenticacao0.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include "sisautenticacao0.hpp"
+
+using namespace std;
+
+// Definições de constantes.
+
+//const string StubISAutenticacao::TRIGGER_FALHA;
+//const string StubISAutenticacao::TRIGGER_ERRO_SISTEMA;
+
+// Implementação de método.
+
+bool StubISAutenticacao0::autenticar(const Codigo &codigo, const Senha &senha){
+
+    // Apresentar dados recebidos.
+
+    cout << endl << "StubISAutenticacao::autenticar" << endl ;
+
+    cout << "Codigo = " << codigo.getValor() << endl ;
+    cout << "Senha = " << senha.getValor() << endl ;
+
+    // Diferentes comportamentos dependendo do valor da matrícula.
+
+    /*switch(codigo.getValor()){
+        case TRIGGER_FALHA:
+            return false;
+        case TRIGGER_ERRO_SISTEMA:
+            throw runtime_error("Erro de sistema");
+        }*/
+    if (codigo.getValor() == TRIGGER_FALHA) {
+        return false;
+    } else
+    if (codigo.getValor() == TRIGGER_ERRO_SISTEMA) {
+        throw runtime_error("Erro de sistema");
+    }
+
+    return true;
+}
